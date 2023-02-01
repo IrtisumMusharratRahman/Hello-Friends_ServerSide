7cd1f0ea866b43e35633b0d8863a399225e65d5b
IrtisumMusharratRahman <rhythmfakir@gmail.com>
2023-02-01 18:33:02 +0600

    initial commit

---
+++ hellofrields/src/main/java/com/server/hellofrields/HellofrieldsApplication.java
@@ -0,0 +1,13 @@
+package com.server.hellofrields;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellofrieldsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellofrieldsApplication.class, args);
+	}
+
+}
---
+++ hellofrields/src/test/java/com/server/hellofrields/HellofrieldsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.hellofrields;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellofrieldsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
